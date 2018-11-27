a06d1693c6bf67c7786ee2c8dc1989ce9b2b9614
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2018-11-27 13:38:55 -0600

    adding and commiting this to keep the commit logs in the python file updated

--- app/urls.py
+++ app/urls.py
@@ -7,25 +7,31 @@ urlpatterns = [
     path(
         'todo/',
         views.todos,
-        name='todos', ),
+        name='todos',
+    ),
     path(
         'todo/create/',
         views.create_todo,
-        name='create', ),
+        name='create',
+    ),
     path(
         'todo/<int:id>/toggle_editing/',
         views.toggle_editing,
-        name='toggle_editing', ),
+        name='toggle_editing',
+    ),
     path(
         'todo/<int:id>/toggle_completed/',
         views.toggle_completed,
-        name='toggle_completed', ),
+        name='toggle_completed',
+    ),
     path(
         'todo/<int:id>/new-title',
         views.new_title,
-        name='new-title', ),
+        name='new-title',
+    ),
     path(
         'todo/<int:id>/delete/',
         views.delete_todo,
-        name='delete', )
+        name='delete',
+    )
 ]
